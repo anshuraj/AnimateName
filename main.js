14186a1719edfd637a8f98de8a1a71f69b49611a
Anshu Raj <imanshuraj@gmail.com>
2015-02-21 00:49:53 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var green = [75, 100, 40];
 var blue = [196, 77, 55];
 var purple = [280, 50, 60];
 
-var myName = "Anshu Raj";
+var myName = prompt("Please enter your name ");
 var letterColors = [red, orange, green]
 var bubbleShape = "square";
 if(10>3){
